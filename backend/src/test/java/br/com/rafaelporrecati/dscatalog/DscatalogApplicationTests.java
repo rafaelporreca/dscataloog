637598df8482b1b3d7b5c1209425886a8e48cde1
Rafael Porreca <rafael.porreca@gmail.com>
2021-03-18 18:43:44 -0300

    Projeto criado

---
+++ backend/src/test/java/br/com/rafaelporrecati/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelporrecati.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
