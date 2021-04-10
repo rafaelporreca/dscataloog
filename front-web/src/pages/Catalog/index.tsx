5f1dc827d6b2ada5f0a7e45630f1dd2b65447c05
Rafael Porreca <rafael.porreca@gmail.com>
2021-04-10 10:59:54 -0300

    Finish product listing layout

--- front-web/src/pages/Catalog/index.tsx
+++ front-web/src/pages/Catalog/index.tsx
@@ -1,8 +1,22 @@
 import React from 'react';
+import ProductCard from './components/ProductCard';
+import './styles.scss';
 
 const Catalog = () =>(
-    <div>
-        <h1>Catálogo</h1>
+    <div className="catalog-container">
+        <h1 className="catalog-title">Catálogo de produtos</h1>
+        <div className="catalog-products">
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+        <ProductCard/>
+    
+        </div>
     </div>
 );
 
