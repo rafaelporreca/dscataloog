86e5e2deda8fb9b0691d4cc864b77e161c4bcad7
Rafael Porreca <rafael.porreca@gmail.com>
2021-05-19 18:02:49 -0300

    Postgres compability

--- backend/src/main/java/br/com/rafaelporrecati/dscatalog/servicies/ProductService.java
+++ backend/src/main/java/br/com/rafaelporrecati/dscatalog/servicies/ProductService.java
@@ -17,6 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -32,8 +34,9 @@ public class ProductService {
     }
 
     @Transactional(readOnly = true)
-    public Page<ProductDTO> findAllPaged(PageRequest pageRequest){
-        Page<Product> list = productRepository.findAll(pageRequest);
+    public Page<ProductDTO> findAllPaged(Long categoryId, String name,  PageRequest pageRequest){
+        List<Category> categories = (categoryId == 0) ? null : Arrays.asList(categoryRepository.getOne(categoryId));
+        Page<Product> list = productRepository.find(categories, name,  pageRequest);
         Page<ProductDTO> listDto = list.map(obj -> new ProductDTO(obj));
         return listDto;
     }
