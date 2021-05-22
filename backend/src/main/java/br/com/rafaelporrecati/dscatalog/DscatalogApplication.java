8623caaeff9b2d9348f79ea1dcd1592aa82f5e6c
Rafael Porreca <rafael.porreca@gmail.com>
2021-05-22 08:54:01 -0300

    First upload test

--- backend/src/main/java/br/com/rafaelporrecati/dscatalog/DscatalogApplication.java
+++ backend/src/main/java/br/com/rafaelporrecati/dscatalog/DscatalogApplication.java
@@ -1,13 +1,23 @@
 package br.com.rafaelporrecati.dscatalog;
 
+import br.com.rafaelporrecati.dscatalog.servicies.S3Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DscatalogApplication {
+public class DscatalogApplication implements CommandLineRunner {
+
+    @Autowired
+    private S3Service s3Service;
 
     public static void main(String[] args) {
         SpringApplication.run(DscatalogApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        s3Service.uploadFile("c:\\temp\\logo.jpg");
+    }
 }
