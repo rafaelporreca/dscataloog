3a273023fd5acdf44984648ab63cfe25b93a1c23
Rafael Porreca <rafael.porreca@gmail.com>
2021-08-01 09:12:32 -0300

    Make login and save user session data

--- front-web/src/pages/Auth/components/Login/index.tsx
+++ front-web/src/pages/Auth/components/Login/index.tsx
@@ -1,25 +1,44 @@
-import React from "react";
+import React, {useState} from "react";
 import './styles.scss'
 import AuthCard from "../Card";
-import { Link } from "react-router-dom";
-import { useForm } from 'react-hook-form';
+import {Link, useHistory} from "react-router-dom";
+import {useForm} from 'react-hook-form';
 import Buttonicon from "../../../../core/components/Buttonicon";
+import {makeLogin} from "../../../../core/Utils/request";
+import {saveSessionData} from "../../../../core/Utils/auth";
 
 type FormData = {
-    email: string;
+    username: string;
     password: string;
 }
 
 const Login = () => {
-    const { register, handleSubmit } = useForm<FormData>();
+    const {register, handleSubmit} = useForm<FormData>();
+    const [hasError, setHasError] = useState(false);
+    const history = useHistory();
     const onSubmit = (data: FormData) => {
-        console.log(data);
+        makeLogin(data)
+            .then(response => {
+                setHasError(false);
+                saveSessionData(response.data);
+                history.push('/admin')
+            })
+            .catch(() => {
+                setHasError(true);
+            })
     }
+
+
     return (
         <AuthCard title="login">
-            <form className="login-form"  onSubmit={handleSubmit(onSubmit)}>
-                <input {...register('email')} type="email" className="form-control input-base margin-bottom-30" placeholder="Email"/>
-                <input {...register('password')} type="password" className="form-control input-base" placeholder="Senha"/>
+            {hasError && (<div className="alert alert-danger mt-5">
+                Usuário ou senha inválidos!
+            </div>)}
+            <form className="login-form" onSubmit={handleSubmit(onSubmit)}>
+                <input {...register('username', {required: true})} type="email" className="form-control input-base margin-bottom-30"
+                       placeholder="Email"/>
+                <input {...register('password', {required: true})} type="password" className="form-control input-base"
+                       placeholder="Senha"/>
                 <Link to="/admin/auth/recover" className="login-link-recover">
                     Esqueci a senha?
                 </Link>
